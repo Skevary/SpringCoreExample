2455398a51948116a68b6d7a959759bbd5daf15f
Skevary <Skevarys@gmail.com>
2016-10-31 16:27:23 +0200

    Add NewLine.java(AOP to springidol-project)

---
+++ src/main/java/springIdol/NewLine.java
@@ -0,0 +1,14 @@
+package springIdol;
+
+public class NewLine {
+	
+	public void singleAfterPerform(){
+		System.out.println();
+		
+	}
+	
+	public void singleBeforePerform(){
+		System.out.println();
+	}
+	
+}
