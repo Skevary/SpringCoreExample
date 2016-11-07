71032980b5efe8644e0460589aa86151b46673f8
Skevary <Skevarys@gmail.com>
2016-11-07 11:52:13 +0200

    Add messageSource

--- src/main/java/postprocessor/RabbitMain.java
+++ src/main/java/postprocessor/RabbitMain.java
@@ -1,17 +1,24 @@
 package postprocessor;
 
+import java.util.Locale;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class RabbitMain {
 
+		
 	public static void main(String[] args) {
-
+		
 		@SuppressWarnings("resource")
 		ApplicationContext context = new ClassPathXmlApplicationContext("postprocessor.xml");
 		Rabbit bugsBunny= (Rabbit) context.getBean("bugs");
 		bugsBunny.say();
-
+		
+		Locale locale = new Locale("es","ES");
+		String text = context.getMessage("computer", new Object[0], locale);
+		System.out.println(text);
+		
 	}
 
 }
