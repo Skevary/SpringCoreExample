06691de2f521677cc7f8056548a9892e9753eb9f
Skevary <Skevarys@gmail.com>
2016-11-07 07:09:37 +0200

    Add an example custom editor configurer

---
+++ src/main/java/propeditor/ContactMain.java
@@ -0,0 +1,15 @@
+package propeditor;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ContactMain {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		ApplicationContext context = new ClassPathXmlApplicationContext("prop-editor.xml");
+		Contact contact = (Contact) context.getBean("contact");
+		contact.printContact();
+	}
+
+}
---
+++ src/main/java/propeditor/PhoneNumberEditorRegistrar.java
@@ -0,0 +1,13 @@
+package propeditor;
+
+import org.springframework.beans.PropertyEditorRegistrar;
+import org.springframework.beans.PropertyEditorRegistry;
+
+public class PhoneNumberEditorRegistrar implements PropertyEditorRegistrar {
+
+	@Override
+	public void registerCustomEditors(PropertyEditorRegistry registry) {
+		registry.registerCustomEditor(PhoneNumber.class ,new PhoneEditor());
+	}
+
+}
