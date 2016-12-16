466faedfbb3bde5194937edf09f231c26e9e1c38
Skevary <Skevarys@gmail.com>
2016-12-16 23:53:01 +0200

    Add BeanShell-script

--- src/main/java/scripting/ScriptStarted.java
+++ src/main/java/scripting/ScriptStarted.java
@@ -8,10 +8,12 @@ public class ScriptStarted {
 	System.out.println("Run script...");
 	ruby.show();
 	groovy.show();
+	beanShell.show();
     }
 
     private Script ruby;
     private Script groovy;
+    private Script beanShell;
     
     public void setRuby(Script ruby) {
 	this.ruby = ruby;
@@ -19,5 +21,9 @@ public class ScriptStarted {
 
     public void setGroovy(Script groovy) {
         this.groovy = groovy;
+    }
+
+    public void setBeanShell(Script beanShell) {
+        this.beanShell = beanShell;
     }
 }
