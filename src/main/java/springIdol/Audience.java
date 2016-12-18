4831cdb371d66b8bd713fc60ea5b691a13d47bcf
Skevary <Skevarys@gmail.com>
2016-12-18 06:15:35 +0200

    QuickFix add the target method proceed()

--- src/main/java/springIdol/Audience.java
+++ src/main/java/springIdol/Audience.java
@@ -8,6 +8,7 @@ public class Audience {
 	 System.out.println("The audience is taking their seats.");
 	 System.out.println("The audience is turning off their cellphones");
 	 long start = System.currentTimeMillis(); // Перед выступлением
+	 joinpoint.proceed(); // Вызов целевого метода
 	 long end = System.currentTimeMillis(); // После выступления
 	 System.out.println("CLAP CLAP CLAP CLAP CLAP");
 	 System.out.println("The performance took " + (end - start)
