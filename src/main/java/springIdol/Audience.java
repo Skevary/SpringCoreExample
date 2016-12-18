bbe117fe1b4d96e73e2d56c284ecb27b82b91052
Skevary <Skevarys@gmail.com>
2016-12-18 04:54:59 +0200

    Add aop:around

--- src/main/java/springIdol/Audience.java
+++ src/main/java/springIdol/Audience.java
@@ -1,16 +1,19 @@
 package springIdol;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class Audience {
-    public void takeSeats() { // Перед выступлением
+    public void watchPerformance(ProceedingJoinPoint joinpoint) {
+	 try {
 	 System.out.println("The audience is taking their seats.");
-	 }
-	 public void turnOffCellPhones() { // Перед выступлением
 	 System.out.println("The audience is turning off their cellphones");
-	 }
-	 public void applaud() { // После выступления
+	 long start = System.currentTimeMillis(); // Перед выступлением
+	 long end = System.currentTimeMillis(); // После выступления
 	 System.out.println("CLAP CLAP CLAP CLAP CLAP");
-	 }
-	 public void demandRefund() { // После неудачного выступления
+	 System.out.println("The performance took " + (end - start)
+	 + " milliseconds.");
+	 } catch (Throwable t) {
 	 System.out.println("Boo! We want our money back!");
 	 }
+}
 }
